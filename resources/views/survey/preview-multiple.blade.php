d69209dbff957bc440a549de9d3a7a95aed7a0ef
adi <tiliztiadi@gmail.com>
2025-11-06 17:43:46 +0700

    fix: month name

--- resources/views/survey/preview-multiple.blade.php
+++ resources/views/survey/preview-multiple.blade.php
@@ -157,8 +157,27 @@
             const hours = String(now.getHours()).padStart(2, '0');
             const minutes = String(now.getMinutes()).padStart(2, '0');
             const seconds = String(now.getSeconds()).padStart(2, '0');
-            $("#dateTime").text(`${day}/${month}/${year} ${hours}:${minutes}:${seconds}`);
+            $("#dateTime").text(`${day} ${getMonthName(month)} ${year} ${hours}:${minutes}:${seconds}`);
         }, 1000);
+
+        function getMonthName(month) {
+            let text = [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December'
+            ]
+
+            return text[month - 1];
+        }
     </script>
 </body>
 
--- resources/views/survey/preview-single.blade.php
+++ resources/views/survey/preview-single.blade.php
@@ -26,7 +26,7 @@
         </div>
 
         <!-- Footer running text -->
-       @include('survey._footer')
+        @include('survey._footer')
     </div>
 
     <!-- Modals -->
@@ -173,9 +173,27 @@
                 const hours = String(now.getHours()).padStart(2, '0');
                 const minutes = String(now.getMinutes()).padStart(2, '0');
                 const seconds = String(now.getSeconds()).padStart(2, '0');
-                $("#dateTime").text(`${day}/${month}/${year} ${hours}:${minutes}:${seconds}`);
+                $("#dateTime").text(`${day} ${getMonthName(month)} ${year} ${hours}:${minutes}:${seconds}`);
             }, 1000);
 
+            function getMonthName(month) {
+                let text = [
+                    'January',
+                    'February',
+                    'March',
+                    'April',
+                    'May',
+                    'June',
+                    'July',
+                    'August',
+                    'September',
+                    'October',
+                    'November',
+                    'December'
+                ]
+
+                return text[month - 1];
+            }
         });
     </script>
 </body>
